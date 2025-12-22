a404eaadfdddf0d5b8493761fe6a0d3c2a0cc2c4
Mark Erikson <mark@isquaredsoftware.com>
2025-12-22 17:36:32 -0500

    Fix exported type errors
    - Exported ISource
    - Added `ReactStore` interface
    - Made `Store` fields not private to fix TS4094 error

--- packages/use-store/src/experimental/index.ts
+++ packages/use-store/src/experimental/index.ts
@@ -5,3 +5,8 @@ export {
   createStoreFromSource,
   StoreProvider,
 } from "./useStore";
+
+// Export types needed for public API
+export type { ISource, Reducer } from "../types";
+export type { ReactStore } from "./Store";
+export { Store } from "./Store";
